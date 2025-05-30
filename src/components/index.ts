6376e48ae6a823706fea368232ae0df9d590f64d
crackerky <crackert.ky@gmail.com>
2025-05-30 17:04:37 +0900

    📦 Add components index for easy imports

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+// Atoms
+export { default as BackgroundAnimation } from './atoms/BackgroundAnimation'
+export { Typography } from './atoms/Typography'
+export { default as Badge } from './atoms/Badge'
+export { default as Button } from './atoms/Button'
+export { default as Card } from './atoms/Card'
+export { default as Divider } from './atoms/Divider'
+export { default as Icon } from './atoms/Icon'
+export { default as Input } from './atoms/Input'
+export { default as Logo } from './atoms/Logo'
+export { default as Triangle } from './atoms/Triangle'
+
+// Templates  
+export { default as DemoPageTemplate } from './templates/DemoPageTemplate'
