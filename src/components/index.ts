9283941f60b3a6b557dbd2d147d467a6a97be9ce
crackerky <crackert.ky@gmail.com>
2025-05-30 17:13:13 +0900

    🐛 Fix component exports - Use correct named exports instead of default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,14 @@
 // Atoms
 export { default as BackgroundAnimation } from './atoms/BackgroundAnimation'
 export { Typography } from './atoms/Typography'
-export { default as Badge } from './atoms/Badge'
+export { Badge } from './atoms/Badge'
 export { Button } from './atoms/Button'
 export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './atoms/Card'
-export { default as Divider } from './atoms/Divider'
-export { default as Icon } from './atoms/Icon'
+export { Divider } from './atoms/Divider'
+export { Icon } from './atoms/Icon'
 export { Input } from './atoms/Input'
-export { default as Logo } from './atoms/Logo'
-export { default as Triangle } from './atoms/Triangle'
+export { Logo } from './atoms/Logo'
+export { Triangle } from './atoms/Triangle'
 
 // Templates  
 export { default as DemoPageTemplate } from './templates/DemoPageTemplate'
